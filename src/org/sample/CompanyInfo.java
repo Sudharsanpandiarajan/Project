baa8748ca4933eedc3f10a0003884314072c77f2
Sudharsanpandiarajan <mr.sudharsanofficial@gmail.com>
2022-10-06 00:03:24 +0530

    Step definition

---
+++ src/org/sample/CompanyInfo.java
@@ -0,0 +1,29 @@
+package org.sample;
+
+//g parent class
+public class CompanyInfo {
+
+	private String company() {
+		
+		String name = "harish";
+	System.out.println(name);
+		
+		return name;
+
+	}
+	public static void main(String[] args) {
+		
+		CompanyInfo c = new CompanyInfo();
+		
+		c.company();
+		
+		
+		
+		
+		
+	}
+		
+
+	}
+
+
